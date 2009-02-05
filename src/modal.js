020dcf962602e3048378143a5e112de7ac694a8b
Prateek Saxena <prtksxna@gmail.com>
2009-02-05 13:41:12 +0530

    Added an option to remove the overlay

--- src/modal.js
+++ src/modal.js
@@ -3,24 +3,22 @@ ModalWindow = Class.create({
 		this.cont = "";
 		this.overlay = "";
 		this.win = "";
+		this.container = new Element('div', {id:'modal-container'});
+		var container = this.container;
+		$(document.body).insert({bottom:container});
 	},
-	show: function(content){
-		this.cont = content;
-		this.overlay = $("modal-container").appendChild(new Element('div', {'class':'modal-overlay'}));
-		this.win = $("modal-container").appendChild(new Element('div', {'class':'modal-window'}));
-		var data = this.win.update(this.cont + "<span id=\'modal-close-btn\'>close<\/span>");
+	show: function(element, overlay){
+		this.close();
+		this.cont = element;
+		if(overlay) this.overlay = this.container.appendChild(new Element('div', {'class':'modal-overlay'}));
+		this.win = this.container.appendChild(new Element('div', {'class':'modal-window'}));
+		this.win.insert({bottom:this.cont});
+		var closeBtn = new Element('span',{id:'modal-close-btn'}).update("[Close]");
+		this.win.insert({bottom:closeBtn});
 		$("modal-close-btn").observe('click', this.close.bind(this));
 	},
 	close: function(e){
 		if(e) e.stop();
-		$("modal-container").childElements().invoke('remove');
+		this.container.childElements().invoke('remove');
 	}
-});
-
-get = new ModalWindow();
-
-Event.observe(window, 'load', function() {
-	$("get_win").observe('click',function(e) {
-		get.show("Hello. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Hello. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Hello. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Hello. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-	})
 });
